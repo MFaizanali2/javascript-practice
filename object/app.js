b458a9fcf528d41def0fbe24aae18a077fc6148e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-25 21:46:35 +0500

    js line 55
    continue

--- object/app.js
+++ object/app.js
@@ -1,56 +1,56 @@
-// let cars = [
-//     {
-//       "brand": "Toyota",
-//       "famousModel": "Camry",
-//       "color": "Super White"
-//     },
-//     {
-//       "brand": "Ford",
-//       "famousModel": "Mustang",
-//       "color": "Race Red"
-//     },
-//     {
-//       "brand": "BMW",
-//       "famousModel": "3 Series",
-//       "color": "Alpine White"
-//     },
-//     {
-//       "brand": "Mercedes-Benz",
-//       "famousModel": "C-Class",
-//       "color": "Obsidian Black"
-//     },
-//     {
-//       "brand": "Honda",
-//       "famousModel": "Civic",
-//       "color": "Crystal Black Pearl"
-//     },
-//     {
-//       "brand": "Tesla",
-//       "famousModel": "Model S",
-//       "color": "Midnight Silver Metallic"
-//     },
-//     {
-//       "brand": "Audi",
-//       "famousModel": "A4",
-//       "color": "Ibis White"
-//     },
-//     {
-//       "brand": "Chevrolet",
-//       "famousModel": "Corvette",
-//       "color": "Torch Red"
-//     },
-//     {
-//       "brand": "Nissan",
-//       "famousModel": "Altima",
-//       "color": "Gun Metallic"
-//     },
-//     {
-//       "brand": "Hyundai",
-//       "famousModel": "Elantra",
-//       "color": "Phantom Black"
-//     }
-//   ]
+let cars = [
+    {
+      "brand": "Toyota",
+      "famousModel": "Camry",
+      "color": "Super White"
+    },
+    {
+      "brand": "Ford",
+      "famousModel": "Mustang",
+      "color": "Race Red"
+    },
+    {
+      "brand": "BMW",
+      "famousModel": "3 Series",
+      "color": "Alpine White"
+    },
+    {
+      "brand": "Mercedes-Benz",
+      "famousModel": "C-Class",
+      "color": "Obsidian Black"
+    },
+    {
+      "brand": "Honda",
+      "famousModel": "Civic",
+      "color": "Crystal Black Pearl"
+    },
+    {
+      "brand": "Tesla",
+      "famousModel": "Model S",
+      "color": "Midnight Silver Metallic"
+    },
+    {
+      "brand": "Audi",
+      "famousModel": "A4",
+      "color": "Ibis White"
+    },
+    {
+      "brand": "Chevrolet",
+      "famousModel": "Corvette",
+      "color": "Torch Red"
+    },
+    {
+      "brand": "Nissan",
+      "famousModel": "Altima",
+      "color": "Gun Metallic"
+    },
+    {
+      "brand": "Hyundai",
+      "famousModel": "Elantra",
+      "color": "Phantom Black"
+    }
+  ]
 
-//   for(let keys in cars){
-//     console.log(cars[keys].famousModel)
-//   }
+  for(let keys in cars){
+    console.log(cars[keys].famousModel)
+  }
