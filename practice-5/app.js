285947923d9c5967f12da4b36850c0757f3d287f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-25 19:27:00 +0500

    Update app.js

---
+++ practice-5/app.js
@@ -0,0 +1,59 @@
+   // alert("hello faizi");
+            // let sum = 0;
+            // let n = 1000;
+            // for(let i = 1; i<= 5; i++){
+            //     console.log("i =" ,i);
+            // }
+            
+            // let i = 1;
+            // while(i<=10){
+            //     console.log("hello faizi");
+            //     i++
+            // }
+
+            //     sum = sum + i;
+            // console.log("sum = ",sum);
+            // console.log("loop has ended");
+
+            // var faizi = ["shayan faizan hasnain" ,87,];
+            // faizi.push("ali rahman saleem")
+            
+            // // myArray.push("araiz");
+            // document.write(faizi);
+
+            // var mark = [98,76,87,44,67,65];
+            // let sum = 0;
+            // for(let value of mark){
+            //     sum += value
+            // }
+            // let avg = sum / mark.length;
+            // console.log('average marks of the class =', avg);
+
+
+            // console.log(mark);
+            // console.log(mark.length);
+            // var hero = ["spiderman antman badman thor ironman hulk ben messi"];
+            // for(let idx=0; idx<hero.length; idx++){
+            //     console.log(hero[idx]);
+            // }
+            // let cities = ["karachi hyderabad lahor sibi quetta thar multan peshawar"];
+            // for(let city of cities){
+            //     console.log(city.toUpperCase());
+            // }
+
+                // let item = ["apple", "chips", "burger", "juice", "biryani"];
+                // console.log(item);
+                // console.log(item.toString());
+                // console.log(item);
+
+                // let marvel = ["hulk", "thor", "Spiderman", "antman"];
+                // let dcHeroes = ["batman", "superman"];
+                // let hero = marvel.concat(dcHeroes);
+                // console.log(hero);
+                // marvel.unshift("mrbreast");
+                // console.log(marvel);
+                // marvel.shift();
+                // console.log(marvel);
+                // console.log(marvel.slice(1,4));
+
+                let brand = ["bloomberg", "Microsoft", "Uber", "google","ibm","Netflix"];
