60a09028dca308ca368a59dba5c44306df76f02e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-25 22:46:35 +0500

    Update app.js

--- ES6/app.js
+++ ES6/app.js
@@ -26,7 +26,29 @@ let obj = [
     }
 ];
 
-obj.filter((sum) =>{
-    return sum.name ;
-    console.log(sum)
-})
+// let abc = obj.filter((sum) =>{
+//     return sum.name === "faizan" ;
+    // console.log(sum)
+// });
+// console.log(abc);
+
+// let data = obj.find((sum) => {
+//     return sum.name === "faizan"
+// });
+// console.log(data)
+
+
+
+
+
+let data = [45, 56, 7, 23, 9, 6, 99, 103, 167, 75]
+
+let returndata = data.filter((item) => {
+    return item < 10
+}) 
+console.log(returndata);
+
+let returndata1 = data.find((item) => {
+    return item > 10
+}) 
+console.log(returndata1);
