b30336c5c86860bf2f0f6c56a3cce749840a7ca9
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-25 19:19:45 +0500

    Update app.js

---
+++ practice-3/app.js
@@ -0,0 +1,42 @@
+
+for (let i = 0; i < 100; i += 5) {
+    console.log(i);
+
+    if (i === 25) {
+        console.log("ha, faizi");
+        break;
+    }
+    if (i === 15) {
+        console.log("ha, faizi");
+        break;
+    }
+    console.log("this is loop", i);
+}
+
+
+// for(var i = 1; i<=40; i++){
+//     if(i % 2  === 0){
+//         console.log(i,"Even number");
+//     }else{
+//         console.log(i,"Odd Number");
+//     }
+// }
+
+// function isPrime(num) {
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             return false;
+//         }
+//     }
+//     return num > 1;
+// }
+
+// for (let i = 1; i < 1000; i++) {
+//     if (isPrime(i)) {
+//         console.log(i);
+//     }
+// }
+
+// for(var i=5; i<=10; i++){
+//     console.log(i)
+// }
