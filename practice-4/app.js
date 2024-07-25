f12cc04154e0ebdc23ee190c4e7f0d8702c25027
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-25 19:24:36 +0500

    Update app.js

---
+++ practice-4/app.js
@@ -0,0 +1,28 @@
+  // let a = 6;
+            // let b = 5;
+
+            // let con1 = a > b;
+            // let con2 = a === 6;
+            // console.log("con1 && con2", con1 && con2);
+
+            const name = "Faizan";
+            const age = 19;
+            console.log(`Hello my name is ${name} and my age is ${age}`);
+
+            const gameName = new String('Faizan');
+            console.log(gameName[0]);
+            console.log(gameName._proto_);
+            console.log(gameName);
+
+            console.log(gameName.length);
+            console.log(gameName.toUpperCase());
+            console.log(gameName.charAt("z"));
+            console.log(gameName.slice(0,3));
+            console.log(gameName.indexOf('z'));
+            console.log(gameName.lastIndexOf("f"));
+
+            const newone = "  Faizan   ";
+            console.log(newone);
+            console.log(newone.trim());
+
+            console.log(name)
