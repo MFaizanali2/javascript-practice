3256185fa48cf147eeee92df4db07ab5852f5fb7
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-07-25 19:14:49 +0500

    Update app.js

---
+++ practice-1/app.js
@@ -0,0 +1,18 @@
+var first = +prompt("Enter First Value");
+    var second = +prompt("Enter second Value");
+        var opt = prompt("Enter operator");
+            
+    if(opt = first + second){
+           console.log(first+second);
+    }
+     if(opt = first - second){
+          console.log(first-second);
+    }
+    if(opt = first * second){
+       console.log(first*second);
+    }
+    if(opt = first / second){    
+    console.log(first/second);  
+  }
+
+
